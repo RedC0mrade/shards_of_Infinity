5610db21d7046597c387be5fa527805683a4b0c9
RedC0mrade <e2e4e2e4@mail.ru>
2025-04-25 19:58:04 +0200

    ---

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        case_sensitive=False,
+    )
+
+    bot_token: str = os.getenv("BOT_TOKEN")
+
+
+settings = Settings()
--- main.py
+++ main.py
@@ -1,24 +1,52 @@
 import asyncio
 import logging
 
+from aiogram.client.default import DefaultBotProperties
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart, Command
-from config import BOT_TOKEN
+from aiogram.utils import markdown
+from aiogram.enums import ParseMode
 
+from config import settings
 
-bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 
 @dp.message(CommandStart())
-async def handel_start(message: types.Message):
-    await message.answer(text=f"Hello, {message.from_user.full_name}!")
+async def handle_start(message: types.Message):
+    url = "https://cf.geekdo-images.com/GfQBoqOl21zQx-Vy-dSHZw__imagepagezoom/img/5cxdPGDSaEhGGm1lLkFGKt3-2iE=/fit-in/1200x900/filters:no_upscale():strip_icc()/pic4064509.png"
+    await message.answer(
+        text=f"{markdown.hide_link(url)}Hello, {markdown.hbold(message.from_user.full_name)}!",
+        parse_mode=ParseMode.HTML,
+    )
 
 
 @dp.message(Command("help"))
 async def handel_help(message: types.Message):
-    text = "I'm echo bot.\nSend me any"
+
+    text = markdown.text(
+        markdown.markdown_decoration.quote("I'm an echo bot."),
+        markdown.text(
+            markdown.markdown_decoration.italic(markdown.bold("Send")),
+            markdown.markdown_decoration.quote(" me any!"),
+        ),
+        sep="\n",
+    )
     await message.answer(text=text)
+
+
+# @dp.message(Command("code"))
+# async def handle_command_code(message: types.Message):
+#     text = markdown.text(
+#         "Here`s Python code:",
+#         "",
+#         markdown.pre(
+#             markdown.markdown_decoration.quote("print('Hello world!')"),
+#             "python",
+#         ),
+#         sep="\n",
+#     )
+#     await message.answer(text=text)
 
 
 @dp.message()
@@ -30,7 +58,11 @@ async def echo_message(message: types.Message):
 
 
 async def main():
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
+    bot = Bot(
+        token=settings.bot_token,
+        default=DefaultBotProperties(parse_mode=ParseMode.MARKDOWN_V2),
+    )
     await dp.start_polling(bot)
 
 
