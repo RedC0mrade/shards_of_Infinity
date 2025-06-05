ab49f46998e207d8fcdf93042b0a0ada826a2008
RedC0mrade <e2e4e2e4@mail.ru>
2025-06-05 21:38:23 +0200

    -----------

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from re import Match
 from aiogram.client.default import DefaultBotProperties
 from aiogram import F, Bot, Dispatcher, types
 from aiogram.filters import CommandStart, Command
-from aiogram.utils import markdown
 from aiogram.enums import ChatAction, ParseMode
 from magic_filter import RegexpMode
 
@@ -27,57 +26,58 @@ async def send_text_file(message: types.Message):
     )
 
 
-@dp.message(CommandStart())
+@dp.message(CommandStart())  # CommandStart() Команда /start
 async def handle_start(message: types.Message):
-    url = "https://cf.geekdo-images.com/GfQBoqOl21zQx-Vy-dSHZw__imagepagezoom/img/5cxdPGDSaEhGGm1lLkFGKt3-2iE=/fit-in/1200x900/filters:no_upscale():strip_icc()/pic4064509.png"
+    """Команда /start передает картинку"""
+    url = (
+        "https://cf.geekdo-images.com/GfQBoqOl21zQx-Vy-dSHZw__imagepagezoom/"
+        "img/5cxdPGDSaEhGGm1lLkFGKt3-2iE=/fit-in/1200x900/filters:no_upscale():"
+        "strip_icc()/pic4064509.png"
+    )
     await message.answer(
-        text=f"{markdown.hide_link(url)}Hello, {markdown.hbold(message.from_user.full_name)}!",
-        parse_mode=ParseMode.HTML,
-    )
-
-
-@dp.message(Command("help"))
-async def handel_help(message: types.Message):
-
-    text = markdown.text(
-        markdown.markdown_decoration.quote("I'm an echo bot."),
-        markdown.text(
-            markdown.markdown_decoration.italic(markdown.bold("Send")),
-            markdown.markdown_decoration.quote(" me any!"),
-            ParseMode=ParseMode.MARKDOWN_V2,
+        text=(
+            f'<a href="{url}">&#8205;</a>'  # скрытая ссылка через zero-width space
+            f"Hello, <b>{message.from_user.full_name}</b>!"
         ),
-        sep="\n",
     )
-    await message.answer(text=text)
 
 
-# @dp.message(F.photo, ~F.caption)
-# async def handle_message_no_caption(message: types.Message):
-#     await message.reply("i can't see photo")
+@dp.message(Command("help"))  # Команда /help
+async def handle_help(message: types.Message):
+    text = "<b>I'm an echo bot.</b>\n" "<i><b>Send me many!</b></i>"
+
+    await message.answer(text=text)  # отправка ответа на текст, команду
 
 
-@dp.message(Command("pic"))
+@dp.message(Command("pic"))  # Команда /pic
 async def handel_command_pic(message: types.Message):
-    url = "https://img.freepik.com/free-vector/sweet-eyed-kitten-cartoon-character_1308-135596.jpg?semt=ais_hybrid&w=740"
-    await message.bot.send_chat_action(
-        chat_id=message.chat.id,
-        action=ChatAction.UPLOAD_PHOTO,
+    url = (
+        "https://img.freepik.com/free-vector/sweet-eyed-kitten-"
+        "cartoon-character_1308-135596.jpg?semt=ais_hybrid&w=740"
+    )
+    await message.bot.send_chat_action(  # используется, чтобы показать пользователю, что бот "печатает" или выполняет действие (например, загружает фото, записывает голосовое или ищет данные).
+        chat_id=message.chat.id,  # для отправки указывается id чата
+        action=ChatAction.UPLOAD_PHOTO,  # Показываем, что загружается фото
     )
     await message.reply_photo(
-        photo=url,
-        caption="cat photo",
+        photo=url,  # Ссылка на фото
+        caption="cat photo",  # Описание фото
     )
 
 
-@dp.message(Command("home_pic"))
+@dp.message(
+    Command(
+        "home_pic"
+    )  # Команда /home_pic вводится вручную, если нужна регистрация
+)  # в панеле команд делается через botFather /setcommand
 async def handel_command_home_pic(message: types.Message):
     file_path = "C:/Users/e2e4e/Pictures/IMG_20240802_183555[1].jpg"
     await message.bot.send_chat_action(
         chat_id=message.chat.id,
         action=ChatAction.UPLOAD_PHOTO,
     )
-    await message.reply_photo(
-        photo=types.FSInputFile(
+    await message.reply_photo(  # reply это ответное сообщение
+        photo=types.FSInputFile(  # Загрузка фото с локального диска
             path=file_path,
         ),
         caption="cat photo",
@@ -101,8 +101,8 @@ async def handel_command_file(message: types.Message):
 
 
 @dp.message(
-    F.photo,
-    ~F.caption,
+    F.photo,  # Ручка куда попадают только отправленные фото
+    ~F.caption,  # Без подписи
 )
 async def handle_message_no_caption(message: types.Message):
     caption = "i can't see photo"
@@ -111,14 +111,16 @@ async def handle_message_no_caption(message: types.Message):
         action=ChatAction.TYPING,
     )
     await message.reply_photo(
-        photo=message.photo[-1].file_id,
-        caption=caption,
+        photo=message.photo[-1].file_id,  # Основное фото всегда последнее
+        caption=caption,  # Устанавливаем собственное описание
     )
 
 
 @dp.message(
-    F.photo,
-    F.caption.contains("please"),
+    F.photo,  # Принимает фото
+    F.caption.contains(
+        "please"
+    ),  # Где в тексте есть слово "please" строгий регистр
 )
 async def handle_message_with_caption_please(message: types.Message):
     await message.bot.send_chat_action(
@@ -128,16 +130,19 @@ async def handle_message_with_caption_please(message: types.Message):
     await message.reply(f"{str(message.caption)} please(0)")
 
 
-@dp.message(F.photo, F.caption)
+@dp.message(
+    F.photo,
+    F.caption,  # Есть подпись
+)
 async def handle_message_with_caption(message: types.Message):
     await message.bot.send_chat_action(
-        chat_id=message.chat.id,
-        action=ChatAction.TYPING,
+        chat_id=message.chat.id,  # id чата
+        action=ChatAction.TYPING,  # Бот передает что набирает текст
     )
     await message.reply(f"{str(message.caption)} contains some text!")
 
 
-@dp.message(F.document | F.video)
+@dp.message(F.document | F.video)  # Принимает документы или видео
 async def handle_message_any_media(message: types.Message):
     await message.bot.send_chat_action(
         chat_id=message.chat.id,
@@ -146,18 +151,22 @@ async def handle_message_any_media(message: types.Message):
     if message.document:
         await message.reply_document(
             document=message.document.file_id,
-            caption="document",
+            caption=f"document {message.document.file_id}",
         )
     elif message.video:
         await message.reply_video(
             video=message.video.file_id,
-            caption="video",
+            caption=f"video {message.video.file_id}",
         )
     else:
         await message.reply("WTf is this?")
 
 
-@dp.message(F.from_user.id.in_({1756123777}), F.text.lower() == "secret")
+@dp.message(
+    F.from_user.id.in_({1756123777}),
+    F.text.lower() == "secret",
+)  # from_user.id.in_({1756123777}) - передается список пользователей,
+# для которых доступна команда secret не чувствительна к регистру
 async def secret_admin_message(message: types.Message):
     await message.bot.send_chat_action(
         chat_id=message.chat.id,
