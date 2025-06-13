513c96d88025026f4f9fda075628f183ab9bb5b2
RedC0mrade <e2e4e2e4@mail.ru>
2025-06-13 22:54:02 +0200

    -----------

--- app/keyboards/common_keyboards.py
+++ app/keyboards/common_keyboards.py
@@ -1,5 +1,9 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
+from aiogram.types import (
+    KeyboardButton,
+    ReplyKeyboardMarkup,
+    KeyboardButtonPollType,
+)
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 class ButtonText:
     HELLO = "Hellow!"
@@ -25,3 +29,38 @@ def start_keyboard():
         resize_keyboard=True,
     )
     return markup
+
+
+def get_on_help_kb() -> ReplyKeyboardMarkup:
+    numbers = [
+        "1️⃣",
+        "2️⃣",
+        "3️⃣",
+        "4️⃣",
+        "5️⃣",
+        "6️⃣",
+        "7️⃣",
+        "8️⃣",
+        "9️⃣",
+        "0️⃣",
+    ]
+    buttons_row = [KeyboardButton(text=num) for num in numbers]
+    # buttons_row.append(buttons_row[0])
+    # buttons_row.append(buttons_row[1])
+    # # buttons_row.append(buttons_row[2])
+    # # buttons_row.pop(0)
+    #
+    # markup = ReplyKeyboardMarkup(
+    #     keyboard=[buttons_row, buttons_row],
+    #     resize_keyboard=True,
+    # )
+    # return markup
+    builder = ReplyKeyboardBuilder()
+    for num in numbers:
+        # builder.button(text=num)
+        builder.add(KeyboardButton(text=num))
+    # builder.adjust(3, 3, 4)
+    builder.adjust(3)
+    builder.row(buttons_row[3], buttons_row[1])
+    builder.add(buttons_row[-1])
+    return builder.as_markup(resize_keyboard=False)
