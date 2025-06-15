bca7bdd20f67fa08d78e1576075d8a7fa0cacab0
RedC0mrade <e2e4e2e4@mail.ru>
2025-06-15 16:21:08 +0200

    98798

--- app/routers/comands.py
+++ app/routers/comands.py
@@ -14,6 +14,8 @@ router = Router(name=__name__)
 @router.message(Command("take_a_card"))
 async def handle_take_f_card(message: types.Message):
     
+
+    
 @router.message(CommandStart())  # CommandStart() Команда /start
 async def handle_start(message: types.Message):
     """Команда /start передает картинку"""
