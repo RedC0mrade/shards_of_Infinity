5efb1b84163af6ae245f1e91522addce78371fae
RedC0mrade <e2e4e2e4@mail.ru>
2025-06-22 21:55:39 +0200

    ----------

--- app/keyboards/inline_keyboards.py
+++ app/keyboards/inline_keyboards.py
@@ -1,7 +1,7 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 random_num_updated_cb_data = "random_num_updated_cb_data"
-
+random_num_start_desctop = "random_num_start_desctop"
 def buld_info_kd():
     tg_channel_bt = InlineKeyboardButton(
     text="Канал",
@@ -11,13 +11,17 @@ def buld_info_kd():
         text="Картинка",
         url="https://i.pinimg.com/736x/ac/15/8e/ac158efaaef7f12cbd837dac7ad0f0fe.jpg"
     )
-    btn_random_site = InlineKeyboardButton(
+    btn_random_start = InlineKeyboardButton(
         text="Start data",
         callback_data=random_num_updated_cb_data,
     )
+    btn_random_start_desktop = InlineKeyboardButton(
+        text="Desctop start",
+        callback_data=random_num_start_desctop,
+    )
 
-    row = [tg_channel_bt, btn_random_site]
-    row_1 = [pic]
+    row = [tg_channel_bt, btn_random_start]
+    row_1 = [pic, btn_random_start_desktop]
     rows = [row, row_1]
     markup = InlineKeyboardMarkup(inline_keyboard=rows)
     return markup
--- app/routers/callback_data.py
+++ app/routers/callback_data.py
@@ -1,23 +1,36 @@
 from random import randint
 from aiogram import Bot, F, Router, types
 from aiogram.filters import CommandStart, Command
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from aiogram.types import (
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    CallbackQuery,
+)
 from aiogram.utils.chat_action import ChatActionSender
 from aiogram.enums import ChatAction
 
 from app.keyboards.inline_keyboards import buld_info_kd
-from app.keyboards.inline_keyboards import random_num_updated_cb_data
+from app.keyboards.inline_keyboards import (
+    random_num_updated_cb_data,
+    random_num_start_desctop,
+)
 
 router = Router(name=__name__)
 
+
 @router.callback_query(F.data == random_num_updated_cb_data)
 async def handel_random_site_callback(callback_query: CallbackQuery):
-    bot_me = await callback_query.bot.me() # Информация о боте
-    # print("Bot_me", bot_me, bot_me.username)
-    # await callback_query.bot.answer_callback_query(
-    #     callback_query_id=callback_query.id,
-    #     url = f"t.me/{bot_me.username}?start={randint(1,100)}" # В команду start передаем случайное число
-    # )
-    await callback_query.answer(
-        url = f"t.me/{bot_me.username}?start={randint(1,100)}" # В команду start передаем случайное число
+    bot_me = await callback_query.bot.me()  # Информация о боте
+    await callback_query.bot.answer_callback_query(
+        callback_query_id=callback_query.id,
+        url=f"t.me/{bot_me.username}?start={randint(1,100)}",  # В команду start передаем случайное число
+    )
+
+
+@router.callback_query(F.data == random_num_start_desctop)
+async def handel_random_start_desktop(callback_query: CallbackQuery):
+    bot_me = await callback_query.bot.me()
+    url = f"https://t.me/{bot_me.username}?start={randint(1, 100)}"
+    await callback_query.message.answer(
+        f"Вот ваша <a href='{url}'>ссылка:</a>, что бы начать!",  # В команду start передаем случайное число для декстопной версии
     )
