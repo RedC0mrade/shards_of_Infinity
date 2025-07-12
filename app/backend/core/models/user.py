78417f56b22b08e6e08820132afb668389a7805e
RedC0mrade <e2e4e2e4@mail.ru>
2025-07-12 22:57:49 +0200

    ----------

---
+++ app/backend/core/models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String
+
+from .base_model import Base
+
+
+class TelegrammUser(Base):
+    tablename = "users"
+
+    telegramm_id: Mapped[int] = mapped_column(Integer)
+    first_name: Mapped[str] = mapped_column(String(20))
+    last_name: Mapped[str] = mapped_column(String(20))
