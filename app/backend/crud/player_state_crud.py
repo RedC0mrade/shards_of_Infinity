3a9de1f82e9822633f52786fcb1101011292a79b
RedC0mrade <e2e4e2e4@mail.ru>
2025-08-16 22:55:37 +0200

    ------------

--- app/backend/crud/player_state_crud.py
+++ app/backend/crud/player_state_crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.backend.schemas.play_state import PlayStateSchema
+from app.backend.schemas.play_state import CreatePlayStateSchema
 
 
 class Player_stateServices:
@@ -9,6 +9,6 @@ class Player_stateServices:
 
     async def create_play_state(
         self,
-        play_data: PlayStateSchema,
+        play_data: CreatePlayStateSchema,
     ):
         pass
--- app/backend/schemas/play_state.py
+++ app/backend/schemas/play_state.py
@@ -1,14 +1,17 @@
 from pydantic import BaseModel
 
 
-class PlayStateSchema(BaseModel):
+class CreatePlayStateSchema(BaseModel):
     game_id: int
     player_id: int
-    health: int 
-    mastery: int 
-    crystals: int 
-    power: int 
-    deck_count: int 
-    discard_count: int 
-    hand_count: int 
-    is_defeated: bool 
+    health: int
+    mastery: int
+    crystals: int
+    power: int
+    deck_count: int
+    discard_count: int
+    hand_count: int
+    is_defeated: bool
+
+class PlayStateSchema(BaseModel):
+    id: int
