9aadeacdd3c83b8855af64428371e51b35125358
RedC0mrade <e2e4e2e4@mail.ru>
2025-08-28 23:00:05 +0200

    -----------------

--- app/telegram_bot/keyboards/game_move_keyboard.py
+++ app/telegram_bot/keyboards/game_move_keyboard.py
@@ -1,2 +1,14 @@
+from aiogram.types import (
+    KeyboardButton,
+    ReplyKeyboardMarkup,
+)
+
+class MoveKBText:
+    MARKET = "Рынок"
+    HAND = "Рука"
+    ATTACK = "Атака Игрока"
+    ATTACK_CHAMPION = "Атака чемпиона"
+    END = "Конец хода"
+    DEFEAT = "Сдаться"
 def in_play_card_keyboard():
     pass
