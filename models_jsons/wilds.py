5fdfe43367c4edb45f8aced234e9944cd3f7a559
RedC0mrade <e2e4e2e4@mail.ru>
2025-10-18 22:22:50 +0200

    ------

--- models_jsons/wilds.py
+++ models_jsons/wilds.py
@@ -7,7 +7,7 @@
     "champion_health": 0,
     "card_type": "ally",
     "faction": "wilds",
-    "icon": "C:/telegram/card_1.jpeg",
+    "icon": "C:/telegram/card_2.jpeg",
     "effects": [
       {
         "action": "healing",
@@ -22,6 +22,110 @@
         "effect_type": "conditional",
         "condition_type": "card_on_table",
         "condition_value": 1
+      }
+    ]
+  },
+  {
+    "name": "Ученик подлесья",
+    "crystals_cost": 1,
+    "description": "Получите 3 здоровья. Единение - получите 5 атаки, если в этот ход сыграли другого Союзника природы.",
+    "shield": 0,
+    "champion_health": 0,
+    "card_type": "ally",
+    "faction": "wilds",
+    "icon": "C:/telegram/card_2.jpeg",
+    "effects": [
+      {
+        "action": "healing",
+        "value": 3,
+        "effect_type": "base",
+        "condition_type": "none",
+        "condition_value": 0
+      },
+      {
+        "action": "attack",
+        "value": 2,
+        "effect_type": "conditional",
+        "condition_type": "card_on_table",
+        "condition_value": 1
+      }
+    ]
+  },
+  {
+    "name": "Ученик подлесья",
+    "crystals_cost": 1,
+    "description": "Получите 3 здоровья. Единение - получите 5 атаки, если в этот ход сыграли другого Союзника природы.",
+    "shield": 0,
+    "champion_health": 0,
+    "card_type": "ally",
+    "faction": "wilds",
+    "icon": "C:/telegram/card_2.jpeg",
+    "effects": [
+      {
+        "action": "healing",
+        "value": 3,
+        "effect_type": "base",
+        "condition_type": "none",
+        "condition_value": 0
+      },
+      {
+        "action": "attack",
+        "value": 2,
+        "effect_type": "conditional",
+        "condition_type": "card_on_table",
+        "condition_value": 1
+      }
+    ]
+  },
+  {
+    "name": "Страж Шардвуда",
+    "crystals_cost": 4,
+    "description": "Получите 2 атаки и возьмите карту. Единение - получите 6 здоровья если в этот ход сыграли другого Союзника природы.",
+    "shield": 0,
+    "champion_health": 0,
+    "faction": "wilds",
+    "card_type": "ally",
+    "icon": "c:/telegram/card_6.jpeg",
+    "effects": [
+      {
+        "action": "attack",
+        "value": 2,
+        "effect_type": "base",
+        "condition_type": "none",
+        "condition_value": 0
+      },
+      {
+        "action": "healing",
+        "value": 6,
+        "effect_type": "conditional",
+        "condition_type": "card_on_table",
+        "condition_value": 0
+      }
+    ]
+  },
+  {
+    "name": "Страж Шардвуда",
+    "crystals_cost": 4,
+    "description": "Получите 2 атаки и возьмите карту. Единение - получите 6 здоровья если в этот ход сыграли другого Союзника природы.",
+    "shield": 0,
+    "champion_health": 0,
+    "faction": "wilds",
+    "card_type": "ally",
+    "icon": "c:/telegram/card_6.jpeg",
+    "effects": [
+      {
+        "action": "attack",
+        "value": 2,
+        "effect_type": "base",
+        "condition_type": "none",
+        "condition_value": 0
+      },
+      {
+        "action": "healing",
+        "value": 6,
+        "effect_type": "conditional",
+        "condition_type": "card_on_table",
+        "condition_value": 0
       }
     ]
   },
@@ -71,6 +175,44 @@
     ]
   },
   {
+    "name": "Споровый клерик",
+    "crystals_cost": 2,
+    "description": "Получите 4 здоровья.",
+    "shield": 0,
+    "champion_health": 0,
+    "faction": "wilds",
+    "card_type": "mercenary",
+    "icon": "c:/telegram/card_7.jpeg",
+    "effects": [
+      {
+        "action": "healing",
+        "value": 4,
+        "effect_type": "base",
+        "condition_type": "none",
+        "condition_value": 0
+      }
+    ]
+  },
+  {
+    "name": "Споровый клерик",
+    "crystals_cost": 2,
+    "description": "Получите 4 здоровья.",
+    "shield": 0,
+    "champion_health": 0,
+    "faction": "wilds",
+    "card_type": "mercenary",
+    "icon": "c:/telegram/card_7.jpeg",
+    "effects": [
+      {
+        "action": "healing",
+        "value": 4,
+        "effect_type": "base",
+        "condition_type": "none",
+        "condition_value": 0
+      }
+    ]
+  },
+  {
     "name": "Рыцарь 3ль'шай",
     "crystals_cost": 3,
     "description": "Получите 3 атаки. Единение - вместо этого получите 6 атаки, если в этот ход сыграли другого Союзника природы.",
@@ -78,7 +220,7 @@
     "champion_health": 0,
     "faction": "wilds",
     "card_type": "mercenary",
-    "icon": "c:/telegram/card_10.jpeg",
+    "icon": "c:/telegram/card_11.jpeg",
     "effects": [
       {
         "action": "attack",
@@ -95,5 +237,109 @@
         "condition_value": 1
       }
     ]
-  }
+  },
+  {
+    "name": "Рыцарь 3ль'шай",
+    "crystals_cost": 3,
+    "description": "Получите 3 атаки. Единение - вместо этого получите 6 атаки, если в этот ход сыграли другого Союзника природы.",
+    "shield": 0,
+    "champion_health": 0,
+    "faction": "wilds",
+    "card_type": "mercenary",
+    "icon": "c:/telegram/card_11.jpeg",
+    "effects": [
+      {
+        "action": "attack",
+        "value": 3,
+        "effect_type": "base",
+        "condition_type": "none",
+        "condition_value": 0
+      },
+      {
+        "action": "attack",
+        "value": 3,
+        "effect_type": "conditional",
+        "condition_type": "card_on_table",
+        "condition_value": 1
+      }
+    ]
+  },
+  {
+    "name": "Рыцарь 3ль'шай",
+    "crystals_cost": 3,
+    "description": "Получите 3 атаки. Единение - вместо этого получите 6 атаки, если в этот ход сыграли другого Союзника природы.",
+    "shield": 0,
+    "champion_health": 0,
+    "faction": "wilds",
+    "card_type": "mercenary",
+    "icon": "c:/telegram/card_11.jpeg",
+    "effects": [
+      {
+        "action": "attack",
+        "value": 3,
+        "effect_type": "base",
+        "condition_type": "none",
+        "condition_value": 0
+      },
+      {
+        "action": "attack",
+        "value": 3,
+        "effect_type": "conditional",
+        "condition_type": "card_on_table",
+        "condition_value": 1
+      }
+    ]
+  },
+  {
+    "name": "Грибной отшельник",
+    "crystals_cost": 3,
+    "description": "Получите 1 могущества.",
+    "shield": 0,
+    "champion_health": 0,
+    "faction": "wilds",
+    "card_type": "mercenary",
+    "icon": "c:/telegram/card_13.jpeg",
+    "effects": [
+      {
+        "action": "might",
+        "value": 1,
+        "effect_type": "base",
+        "condition_type": "none",
+        "condition_value": 0
+      },
+      {
+        "action": "healing",
+        "value": 5,
+        "effect_type": "conditional",
+        "condition_type": "might",
+        "condition_value": 10
+      }
+    ]
+  },
+  {
+    "name": "Грибной отшельник",
+    "crystals_cost": 3,
+    "description": "Получите 1 могущества.",
+    "shield": 0,
+    "champion_health": 0,
+    "faction": "wilds",
+    "card_type": "mercenary",
+    "icon": "c:/telegram/card_13.jpeg",
+    "effects": [
+      {
+        "action": "might",
+        "value": 1,
+        "effect_type": "base",
+        "condition_type": "none",
+        "condition_value": 0
+      },
+      {
+        "action": "healing",
+        "value": 5,
+        "effect_type": "conditional",
+        "condition_type": "might",
+        "condition_value": 10
+      }
+    ]
+  },
 ]
