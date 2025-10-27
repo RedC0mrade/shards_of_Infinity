c0071f42b7f403aaec67d2703d148fb1ae7b1d41
RedC0mrade <e2e4e2e4@mail.ru>
2025-10-27 23:12:10 +0200

    -

--- models_jsons/homodeus.py
+++ models_jsons/homodeus.py
@@ -209,7 +209,7 @@
     ]
   },
   {
-    "name": "Ревкторный дрон",
+    "name": "Реaкторный дрон",
     "crystals_cost": 3,
     "description": "Получите 3 кристала.",
     "shield": 0,
@@ -228,7 +228,7 @@
     ]
   },
   {
-    "name": "Ревкторный дрон",
+    "name": "Реaкторный дрон",
     "crystals_cost": 3,
     "description": "Получите 3 кристала.",
     "shield": 0,
@@ -247,7 +247,7 @@
     ]
   },
   {
-    "name": "Ревкторный дрон",
+    "name": "Реaкторный дрон",
     "crystals_cost": 3,
     "description": "Получите 3 кристала.",
     "shield": 0,
@@ -316,5 +316,85 @@
         "condition_value": 0
       }
     ]
-  }
+  },
+  {
+    "name": "Нумерианские дроны",
+    "crystals_cost": 3,
+    "description": "Истощить: Получите 1 золото. Следующего Чемпиона Синтетики, которого вы наймёте в этот ход, введите сразу в игру.",
+    "shield": 0,
+    "champion_health": 5,
+    "faction": "homodeus",
+    "card_type": "champion",
+    "icon": "c:/telegram/card_70.jpeg",
+    "effects": [
+      {
+        "action": "gain_gold",
+        "value": 1,
+        "effect_type": "exhaust",
+        "condition_type": "none",
+        "condition_value": 0
+      },
+      {
+        "action": "next_champion_enters_play",
+        "value": 1,
+        "effect_type": "exhaust",
+        "condition_type": "faction",
+        "condition_value": "homodeus"
+      }
+    ]
+  },
+  {
+    "name": "Нумерианские дроны",
+    "crystals_cost": 3,
+    "description": "Истощить: Получите 1 золото. Следующего Чемпиона Синтетики, которого вы наймёте в этот ход, введите сразу в игру.",
+    "shield": 0,
+    "champion_health": 5,
+    "faction": "homodeus",
+    "card_type": "champion",
+    "icon": "c:/telegram/card_71.jpeg",
+    "effects": [
+      {
+        "action": "gain_gold",
+        "value": 1,
+        "effect_type": "exhaust",
+        "condition_type": "none",
+        "condition_value": 0
+      },
+      {
+        "action": "next_champion_enters_play",
+        "value": 1,
+        "effect_type": "exhaust",
+        "condition_type": "faction",
+        "condition_value": "homodeus"
+      }
+    ]
+  },
+  {
+    "name": "Разрушитель Оптио",
+    "crystals_cost": 5,
+    "description": "Истощить: Получите 3 атаки. Если у вас 10 или больше здоровья ядра, вместо этого получите 5 атаки.",
+    "shield": 0,
+    "champion_health": 4,
+    "faction": "homodeus",
+    "card_type": "champion",
+    "icon": "c:/telegram/card_71.jpeg",
+    "effects": [
+      {
+        "action": "gain_attack",
+        "value": 3,
+        "effect_type": "exhaust",
+        "condition_type": "core_health_less_than",
+        "condition_value": 10
+      },
+      {
+        "action": "gain_attack",
+        "value": 5,
+        "effect_type": "exhaust",
+        "condition_type": "core_health_greater_or_equal",
+        "condition_value": 10
+      }
+    ]
+  },
+
+
 ]
