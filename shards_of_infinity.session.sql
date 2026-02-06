1bd3613aa7a4c362e5aabb4376be6d1cc08824fa
RedC0mrade <e2e4e2e4@mail.ru>
2026-02-06 22:07:01 +0200

    ---

--- shards_of_infinity.session.sql
+++ shards_of_infinity.session.sql
@@ -2,16 +2,16 @@
 -- SET player_state_id = 5, zone = 'hand'
 -- WHERE id = 27;
 
--- UPDATE player_states
--- SET crystals = 20
--- WHERE id = 2;    
+UPDATE player_states
+SET crystals = 20
+WHERE id = 2;    
 
 -- DELETE FROM games
 -- WHERE id = 2;
 
-UPDATE player_card_instances
-SET player_state_id = 6, zone = 'hand'
-WHERE id = 319;
+-- UPDATE player_card_instances
+-- SET player_state_id = 2, zone = 'hand'
+-- WHERE id = 97;
 
 -- SELECT id, card_id, game_id, zone FROM player_card_instances
 -- WHERE player_state_id = 6 AND card_id = 319 AND zone = 'hand' AND game_id = 3;
