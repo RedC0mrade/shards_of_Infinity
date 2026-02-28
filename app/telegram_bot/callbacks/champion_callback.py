1f02fb12e1efe7a2df86c5d1165af646b17a42fc
RedC0mrade <e2e4e2e4@mail.ru>
2026-02-28 22:03:30 +0200

    ---

--- app/telegram_bot/callbacks/champion_callback.py
+++ app/telegram_bot/callbacks/champion_callback.py
@@ -73,6 +73,9 @@ async def handle_destroy_champion(
     callback: CallbackQuery,
     callback_data: DestroyChampionCallback,
 ):
+    
+    await callback.message.edit_reply_markup(reply_markup=None)
+
     async with db_helper.session_context() as session:
 
         card_instance_service = CardInstanceServices(session=session)
