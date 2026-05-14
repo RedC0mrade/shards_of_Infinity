92b30004c6a418956ac7357630709923ed41c2c0
RedC0mrade <e2e4e2e4@mail.ru>
2026-05-14 23:05:09 +0200

    ---

--- app/backend/crud/actions/game_move.py
+++ app/backend/crud/actions/game_move.py
@@ -1,4 +1,3 @@
-
 from app.backend.core.models.player_state import PlayerState
 from app.backend.crud.base_service import BaseService
 from app.backend.crud.card_instance_crud import CardInstanceServices
